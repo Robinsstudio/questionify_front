5bcc69ff0e8a556318ca59352c964cbafff42836
Robinsstudio <robin_1997@hotmail.fr>
2019-02-03 16:41:46 +0100

    Passage sous create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ExplorerView from './ExplorerView';
+
+class App extends Component {
+	render() {
+		return (
+			<ExplorerView/>
+		);
+	}
+}
+
+export default App;
