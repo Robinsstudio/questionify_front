b36dcadc1ac4d60918c03ed86a8e4dc7b2b4d685
Robinsstudio <robin_1997@hotmail.fr>
2019-02-18 21:13:35 +0100

    Créer un QCM

--- src/App.js
+++ src/App.js
@@ -1,14 +1,87 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import ExplorerView from './ExplorerView';
+import Editor from './Editor';
 import Modals from './Modals';
+import request from './request';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			folder: [],
+			files: [],
+			editor: {
+				visible: false,
+				questions: []
+			}
+		};
+
+		this.requestFolder = this.requestFolder.bind(this);
+		this.create = this.create.bind(this);
+		this.edit = this.edit.bind(this);
+		this.save = this.save.bind(this);
+		this.closeEditor = this.closeEditor.bind(this);
+		this.updateEditor = this.updateEditor.bind(this);
+		this.refresh = this.refresh.bind(this);
+	}
+
+	requestFolder(folder = this.state.folder) {
+		request('FolderRequest', { folder }).then(res => res.json()).then( ({folder, files}) => this.setState({folder, files}) );
+	}
+
+	create() {
+		this.setState({
+			editor: {
+				visible: true,
+				questions: []
+			}
+		});
+	}
+
+	edit(files, file) {
+		request('FilesRequest', { files }).then(res => res.json()).then(questions => {
+			this.setState({
+				editor: {
+					file,
+					visible: true,
+					questions: questions.map((quest, i) => {
+						return { ...quest.data, file: files[i] };
+					})
+				}
+			});
+		});
+	}
+
+	updateEditor(editor) {
+		this.setState({ editor });
+	}
+
+	save(file) {
+		request('FileSave', { file, json: JSON.stringify({ type: 'qcm', questions: this.state.editor.questions.map(quest => quest.file) }) }).then(this.closeEditor);
+	}
+
+	closeEditor() {
+		this.setState((state) => {
+			return { editor: { ...state.editor, visible: false } }
+		}, () => this.refresh() );
+	}
+
+	refresh() {
+		const { editor } = this.state;
+		if (editor.visible) {
+			this.edit(editor.questions.map(quest => quest.file), editor.file);
+		}
+		this.requestFolder();
+	}
+
 	render() {
+		const { folder, files, editor } = this.state;
 		return (
-			<Fragment>
-				<ExplorerView/>
+			<div id="app">
+				<ExplorerView editing={editor.visible} folder={folder} files={files} requestFolder={this.requestFolder} create={this.create} edit={this.edit} refresh={this.refresh}/>
+				<Editor editor={editor} folder={folder} update={this.updateEditor} save={this.save} closeEditor={this.closeEditor}/>
 				{Modals.get()}
-			</Fragment>
+			</div>
 		);
 	}
 }
--- src/ExplorerView.js
+++ src/ExplorerView.js
@@ -8,31 +8,24 @@ class ExplorerView extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			folder: [],
-			files: [],
-			contextMenu: {}
+			contextMenu: { visible: false }
 		};
 
 		this.createFolder = this.createFolder.bind(this);
-		this.requestFolder = this.requestFolder.bind(this);
 		this.handleContextMenu = this.handleContextMenu.bind(this);
 		this.hideContextMenu = this.hideContextMenu.bind(this);
-		this.requestFolder = this.requestFolder.bind(this);
 		
-		this.requestFolder();
+		this.props.requestFolder();
 	}
 
 	createFolder(name) {
-		request('FolderCreate', { folder: this.state.folder.concat(name) }).then( () => this.requestFolder() );
-	}
-	
-	requestFolder(folder = this.state.folder) {
-		request('FolderRequest', { folder }).then(res => res.json()).then( ({folder, files}) => this.setState({folder, files}) );
+		const { folder, requestFolder } = this.props;
+		request('FolderCreate', { folder: folder.concat(name) }).then( () => requestFolder() );
 	}
 
 	goBack(howMany) {
-		const folder = this.state.folder;
-		this.requestFolder( (howMany <= folder.length) ? folder.slice(0, folder.length - howMany) : folder );
+		const { folder, requestFolder } = this.props;
+		requestFolder( (howMany <= folder.length) ? folder.slice(0, folder.length - howMany) : folder );
 	}
 
 	handleContextMenu(event, items = []) {
@@ -46,14 +39,15 @@ class ExplorerView extends Component {
 	}
 
 	buildMenuItems(items) {
-		const { folder } = this.state;
+		const { create, folder, refresh } = this.props;
 		return items.concat(
 			{ label: 'Nouveau dossier', onClick: () => {
 				Modals.showPromptModal('Nouveau dossier', 'Entrez un nom de dossier ici...').then(name => this.createFolder(name)).catch(() => {});
 			}},
 			{ label: 'Nouvelle question', onClick: () => Modals.showQuestionModal({ question: '', answers: [] }).then(quest => {
-				request('FileSave', { file: folder.concat(quest.name), json: JSON.stringify(quest) }).then( () => this.requestFolder() );
-			}).catch(() => {}) }
+				request('FileSave', { file: folder.concat(quest.name), json: JSON.stringify(quest) }).then( () => refresh() );
+			}).catch(() => {}) },
+			{ label: 'Nouveau QCM', onClick: () => create() }
 		);
 	}
 
@@ -62,16 +56,16 @@ class ExplorerView extends Component {
 	}
   
 	buildFileItem(file) {
-		const { requestFolder, handleContextMenu, state: { folder } } = this;
-		return React.createElement(File, { folder, file, requestFolder, handleContextMenu });
+		const { handleContextMenu, props: { folder, edit, requestFolder, refresh } } = this;
+		return <File folder={folder} file={file} edit={edit} requestFolder={requestFolder} refresh={refresh} handleContextMenu={handleContextMenu}/>
 	}
 	
 	render() {
-		const { contextMenu } = this.state;
+		const { props: { editing }, state: { contextMenu } } = this;
 		return (
-			<div id='explorer'>
+			<div id='explorer' className={editing ? 'editing' : ''}>
 				<div id='path'>
-					{[].concat(...['Explorer', ...this.state.folder].map((folder, index, self) => {
+					{[].concat(...['Explorer', ...this.props.folder].map((folder, index, self) => {
 						return [
 							<span onClick={() => this.goBack(self.length - index - 1)}>{folder}</span>,
 							<div/>
@@ -79,8 +73,8 @@ class ExplorerView extends Component {
 					})).slice(0, -1)}
 				</div>
 
-				<div id='files' onClick={this.hideContextMenu}  onContextMenu={this.handleContextMenu}>
-					{this.state.files.map(file => this.buildFileItem(file))}
+				<div id='files' onClick={this.hideContextMenu} onContextMenu={this.handleContextMenu}>
+					{this.props.files.map(file => this.buildFileItem(file))}
 				</div>
 
 				{contextMenu.visible && <ContextMenu {...contextMenu}/>}
