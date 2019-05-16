64d7adc1e8c800a7876281ccb84257b1ed799494
florianIBISC <florian.quentin.michaud@gmail.com>
2019-05-16 09:19:11 +0200

    commit message

--- tests/mongo_tests.js
+++ tests/mongo_tests.js
@@ -1,7 +1,9 @@
 const mocha = require('mocha');
 const assert = require('assert');
 const model = require('../impl');
+const chai = require('chai');
 const fetch = require("node-fetch");
+var MongoObjectID = require("mongodb").ObjectID; 
 
 const request = (url, body) => {
 	return fetch(url, {
@@ -35,19 +37,39 @@ describe("Some tests with mongoDb",function(){
         question4Id = getId();
         question5Id = getId();
     });*/
-
+/*
     it('Create folder',()=>{
         request('CreateFolder', { _id: folder1Id, name: 'Folder 1' }).then(()=>{
             assert(folder1Id.isNew === false);
-            console.log('Yeah');
             done();
         },()=>{throw new Error('Erreur')});
     });
 
-    it('Save Question',()=>{
-        
-    })
-});
+    it('Create question',()=>{
+        request('SaveQuestion', { _id: question1Id,
+             name: 'Question 1', label: 'Is this the first question?',
+              answers: [{ label: 'Yes', correct: true }, { label: 'No', correct: false }],
+               idParent: folder1Id }).then(()=>{
+                    assert(question1Id.isNew === false);
+                    done();
+               });
+
+    });
+*/
+    it('Delete question',()=>{
+        model.createFolder({ _id: folder3Id, name: 'Folder 3', idParent: folder1Id });
+        model.delete(folder3Id).then(() =>{
+            var objToFind     = { _id: new MongoObjectID(folder3Id) };
+            console.log(objToFind._id);
+        })
+        });
+
+        //assert.undefined(objToFind);
+        //chai.assert.isNotNull(objToFind);
+        //chai.assert.isNaN(objToFind);
+
+    });
+
 
 
 
